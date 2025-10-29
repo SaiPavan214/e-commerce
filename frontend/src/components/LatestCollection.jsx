53e231643fe6f98191dbda82eb08102946d78236
Sai Pavan <saipavansurya123.in@gmail.com>
2025-10-29 22:20:35 +0530

    ReUpdate Details

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -17,7 +17,7 @@ const LatestCollection = () => {
       <div className='text-center py-8 text-3xl'>
           <Title text1={'LATEST'} text2={'COLLECTIONS'} />
           <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.
+          Discover the newest additions to our store, featuring the latest trends and styles to keep your wardrobe fresh and up-to-date.
           </p>
       </div>
 
